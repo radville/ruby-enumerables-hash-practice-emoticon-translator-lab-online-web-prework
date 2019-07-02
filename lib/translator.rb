689b53630375c0bb5008737c9e077c9e86008e66
Laura Radville <laura.radville@gmail.com>
2019-07-02 16:54:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,8 +7,8 @@ def load_library(file)
   hash["get_emoticon"] = {}
   hash["get_meaning"] = {}
   lib.each do |pair|
-    hash["get_meaning"][pair[0]] = pair[1]
-    hash["get_emoticon"][pair[1]] = pair[0]
+    hash["get_meaning"][pair[1]] = pair[0]
+    hash["get_emoticon"][pair[0]] = pair[1]
 # binding.pry
   end
   hash
