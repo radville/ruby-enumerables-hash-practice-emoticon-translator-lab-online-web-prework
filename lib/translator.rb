e62dea87f81cefcf08885871104a68fc743b18bd
Laura Radville <laura.radville@gmail.com>
2019-07-02 17:00:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(file)
   hash["get_meaning"] = {}
   lib.each do |pair|
     hash["get_meaning"][pair[1][1]] = pair[1][0] # keys are japanese
-    hash["get_emoticon"][pair[1][0]] = pair[1][1] # keys are english
+    hash["get_emoticon"][pair[1][0]] = pair[0] # keys are english
 # binding.pry
   end
   hash
