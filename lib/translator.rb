caf63d2990a546c2dd6efc2650f7f26c1fc264cf
Laura Radville <laura.radville@gmail.com>
2019-07-02 16:50:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,8 @@ require "pry"
 def load_library(file)
   lib = YAML.load_file(file)
   hash = Hash.new(0)
-  hash["get_meaning"] = {}
   hash["get_emoticon"] = {}
+  hash["get_meaning"] = {}
   # lib.each do |pair|
     # lib["get meaning"] = pair[0] 
     binding.pry
